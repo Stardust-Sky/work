4ba0fcb285441187f3a7cfc0bb54603e7740d486
Alex <whitest-alex@yandex.ru>
2021-03-21 17:22:47 +0400

    коммит

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+// {
+// console.log(leftBorderWidth);
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+// console.log(leftBorderWidth);
+
+// let person = {
+//     name: 'john',
+//     age: 25,
+//     isMarried: false
+
+// };
+
+// console.log(person.name);
+// console.log(person['name']);
+
+// let arr = ['plum.png','orange.jpg','apple.png'];
+
+// console.log(arr[2])}
+
+let answer = prompt('Are u sure?');
+console.log(typeof(answer));
